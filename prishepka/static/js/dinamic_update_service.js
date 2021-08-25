9876932d7d9da5cb9234347261aef5e7b7b32dc1
dmitrikovalevski <easylightcheckbox@gmail.com>
2021-08-25 17:38:44 +0300

    добавил модальное окно с полями для добавления новой услуги

---
+++ prishepka/static/js/dinamic_update_service.js
@@ -0,0 +1,25 @@
+'use strict'
+
+$("#update_service").click(function(event) {
+    event.preventDefault();
+    updateService();
+});
+
+
+function updateService() {
+
+};
+
+
+
+
+
+
+
+
+
+
+
+    //let image = document.getElementById("service_image").src.split('/').slice(-3).join('/');
+    //let title = document.getElementById("service_title");
+    //alert(title);
